adae84b12b152ebc4051f197d257bdbb4bfe5976
mandroga <duartefernandes1998@gmail.com>
2025-10-04 16:18:16 +0100

    all update

--- project 2/data_inspection.py
+++ project 2/data_inspection.py
@@ -1,6 +1,11 @@
-from data_imports import *
+# %%
+%run data_imports.py
 
-#df info
-if 1:
-    df.info()
-    print(df.describe().T)
+# %% df info
+print(df.info())
+print(df.shape)
+
+# %%
+print(df.iloc[0, 1].shape)
+
+# %%
